88931b45afcc644da2170af6f235ed0e6c9dfccd
esraa-a11y <esraa.gamal.sim@gmail.com>
2024-01-04 22:13:39 +0200

    create CustomTextField

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
+import 'custom_text_field.dart';
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-      
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(),
+        ],
+      ),
     );
-  }
-}
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
