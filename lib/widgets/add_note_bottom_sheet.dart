be4674db1399d84a02fa4f3ce1d8f026a13a27aa
esraa-a11y <esraa.gamal.sim@gmail.com>
2024-01-07 20:29:26 +0200

    take input and validate data

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -2,33 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'custom_text_field.dart';
-class AddNoteBottomSheet extends StatelessWidget {
+class AddNoteBottomSheet extends StatefulWidget {
   const AddNoteBottomSheet({Key? key}) : super(key: key);
 
   @override
+  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
+}
+
+class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
+    return const Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
 
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 32,
-              ),
-              CustomTextField(hint: 'Title',),
-              SizedBox(
-                height: 16,
-              ),
-              CustomTextField(hint: 'Content',maxLines: 5,),
-              SizedBox(height: 40,),
-              CustomButton(),
-              SizedBox(height: 30,),
-
-            ],
-          ),
+          child: AddNoteForm(),
         ),
 
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+final GlobalKey <FormState> formKey= GlobalKey();
+AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+String? title, subTitle;
+class _AddNoteFormState extends State<AddNoteForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            onSaved: (value){
+              title =value;
+            },
+            hint: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(hint: 'Content',
+            onSaved: (value){
+              subTitle =value;
+            },
+            maxLines: 5,),
+          SizedBox(height: 40,),
+          CustomButton(
+            onTap: (){
+              if(formKey.currentState!.validate()){
+                formKey.currentState!.save();
+              }else{
+                autovalidateMode =AutovalidateMode.always;
+                setState(() {
+
+                });
+              }
+            },
+          ),
+          SizedBox(height: 30,),
+
+        ],
+      ),
     );
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key}) : super(key: key);
-
+  const CustomButton({Key? key, this.onTap}) : super(key: key);
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(8),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        child: Center(child: Text('Save',style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),)),
       ),
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      child: Center(child: Text('Save',style: TextStyle(
-        color: Colors.black,
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-      ),)),
     );
   }
 }
